61a25277e64c05205560df8ddea561a00be24d06
bitterplant <32348388+bitterplant@users.noreply.github.com>
2018-10-12 00:08:20 -0700

    Finished Result class (#12)

--- src/main/java/cs361/battleships/models/Result.java
+++ src/main/java/cs361/battleships/models/Result.java
@@ -1,31 +1,36 @@
 package cs361.battleships.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class Result {
+	@JsonProperty private AtackStatus attackStatus;
+	@JsonProperty private Ship ship;
+	@JsonProperty private Square square;
+
 
 	public AtackStatus getResult() {
-		//TODO implement
-		return null;
+		return this.attackStatus;
 	}
 
 	public void setResult(AtackStatus result) {
-		//TODO implement
+		this.attackStatus = result;
 	}
 
 	public Ship getShip() {
-		//TODO implement
-		return null;
+		return this.ship;
 	}
 
 	public void setShip(Ship ship) {
-		//TODO implement
+		this.ship = ship;
 	}
 
 	public Square getLocation() {
-		//TODO implement
-		return null;
+		return this.square;
 	}
 
 	public void setLocation(Square square) {
-		//TODO implement
+		this.square = square;
 	}
 }
