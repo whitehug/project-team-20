a657f3f0af2eadfbe1c63bc04e8514b5da74b653
zeiglers <32206428+zeiglers@users.noreply.github.com>
2018-10-11 17:14:00 -0700

    Ships implementation (#8)
    * Implementing Ship.java
    
    This contains a constructor for the ship class and a method to access the arrayList of occupied squares
    
    * Delete Ship.java
    
    * Implemented Ship.java
    
    Contains Ship constructors and a method for retrieving the arrayList of occupied squares.
    
    * Implemented fix
    
    Replaced existing code with code from fix posted on the announcement board.
    
    * Tests to ensure ship.java properly creates a ship.
    
    This file creates separate ships of each kind and testing that the proper squares are put into the arrayList.
    
    * Fix to the shipTest file.
    
    This fixes the shipTest to compare both the row and column values of inputted squares.
    
    * Fixed tests
    
    * Fix to handle different casing in ship creation
    
    * Added missing newline.

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -1,19 +1,41 @@
 package cs361.battleships.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
 
 import java.util.List;
 
 public class Ship {
 
 	@JsonProperty private List<Square> occupiedSquares;
-	
+
+	public Ship() {
+
+		occupiedSquares = new ArrayList<>();
+	}
+
 	public Ship(String kind) {
-		//TODO implement
+		kind = kind.toUpperCase();
+		if(kind.equals("BATTLESHIP")){
+
+			occupiedSquares = new ArrayList<>(4);
+		}
+		else if(kind.equals("CRUISER")) {
+
+			occupiedSquares = new ArrayList<>(3);
+		}
+		else if(kind.equals("DESTROYER")) {
+
+			occupiedSquares = new ArrayList<>(2);
+		}
+		else {
+
+			occupiedSquares = new ArrayList<>();
+		}
 	}
 
 	public List<Square> getOccupiedSquares() {
-		//TODO implement
-		return null;
+
+		return occupiedSquares;
 	}
 }
---
+++ src/test/java/cs361/battleships/models/ShipTest.java
@@ -0,0 +1,67 @@
+package cs361.battleships.models;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ShipTest {
+
+    private List<Square> occupiedSquares;
+
+    @Test
+    public void testDestroyerCreation() {
+        Ship ship = new Ship("Destroyer");
+        Square square = new Square(1, 'a');
+        occupiedSquares = ship.getOccupiedSquares();
+        occupiedSquares.add(new Square(1, 'a'));
+        Square square2 = occupiedSquares.get(0);
+        occupiedSquares.add(new Square(1, 'b'));
+        assertTrue(square.getColumn()==square2.getColumn() || square.getRow()==square2.getRow());
+        square.setColumn('b');
+        square2 = occupiedSquares.get(1);
+        assertTrue(square.getColumn()==square2.getColumn() || square.getRow()==square2.getRow());
+    }
+
+    @Test
+    public void testCruiserCreation() {
+        Ship ship = new Ship("CRUISER");
+        Square square = new Square(1, 'a');
+        occupiedSquares = ship.getOccupiedSquares();
+        occupiedSquares.add(new Square(1, 'a'));
+        Square square2 = occupiedSquares.get(0);
+        occupiedSquares.add(new Square(1, 'b'));
+        occupiedSquares.add(new Square(1, 'c'));
+        assertTrue(square.getColumn()==square2.getColumn() || square.getRow()==square2.getRow());
+        square.setColumn('b');
+        square2 = occupiedSquares.get(1);
+        assertTrue(square.getColumn()==square2.getColumn() || square.getRow()==square2.getRow());
+        square.setColumn('c');
+        square2 = occupiedSquares.get(2);
+        assertTrue(square.getColumn()==square2.getColumn() || square.getRow()==square2.getRow());
+    }
+
+    @Test
+    public void testBattleshipCreation() {
+        Ship ship = new Ship("battleship");
+        Square square = new Square(1, 'a');
+        occupiedSquares = ship.getOccupiedSquares();
+        occupiedSquares.add(new Square(1, 'a'));
+        Square square2 = occupiedSquares.get(0);
+        occupiedSquares.add(new Square(1, 'b'));
+        occupiedSquares.add(new Square(1, 'c'));
+        occupiedSquares.add(new Square(1, 'd'));
+        assertTrue(square.getColumn()==square2.getColumn() || square.getRow()==square2.getRow());
+        square.setColumn('b');
+        square2 = occupiedSquares.get(1);
+        assertTrue(square.getColumn()==square2.getColumn() || square.getRow()==square2.getRow());
+        square.setColumn('c');
+        square2 = occupiedSquares.get(2);
+        assertTrue(square.getColumn()==square2.getColumn() || square.getRow()==square2.getRow());
+        square.setColumn('d');
+        square2 = occupiedSquares.get(3);
+        assertTrue(square.getColumn()==square2.getColumn() || square.getRow()==square2.getRow());
+    }
+}
